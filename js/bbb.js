d528486b512db6eb1733f7e4f8dfe768cd830429
不会写shell的小白 <47105171+Smile-C@users.noreply.github.com>
2019-11-16 14:49:45 +0800

    Add files via upload

---
+++ js/bbb.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	var oBtn = document.getElementById("submit");
+
+	oBtn.addEventListener("click", function() {
+		var address = document.getElementById("input_address").value;
+		if (address == "") {
+			alert("你没有填写地址哦")
+		} else {
+			alert("你希望" + address + "天晴");
+		}
+	})
+	oBtn.addEventListener("click", function() {
+		alert("许愿不花钱,也没有晴女");
+	})
+	oBtn.addEventListener("click", function() {
+		alert("仅此以作纪念");
+	})
+
+}
